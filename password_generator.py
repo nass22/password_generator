138dc9fbeb4f0512f092e121305c7eaf7c0a8ccc
Nacer Samir <84498447+nass22@users.noreply.github.com>
2023-04-11 14:04:46 +0200

    simple password generator

--- password_generator.py
+++ password_generator.py
@@ -2,3 +2,21 @@ import random
 
 print("Welcome to Password Generator")
 
+
+chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%&*().,?0123456789"
+
+number = input("How many passwords do you want to generate: ")
+number = int(number)
+
+length = input('Your password length: ')
+length = int(length)
+
+print()
+print("Here are yours passwords:")
+
+for pwd in range(number):
+    passwords = ""
+    for c in range(length):
+        passwords += random.choice(chars)
+    
+    print(passwords)
